8046bdc5809be9ec3b556671a95403d55532f034
venki-tech <32933738+venki-tech@users.noreply.github.com>
2021-03-03 00:26:22 +0000

    modified region

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,4 +1,4 @@
-
+	
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
   owners = ["amazon"]
@@ -18,13 +18,13 @@ data "aws_ami" "amazon-linux-2" {
 
 resource "aws_instance" "jenkins-instance" {
   ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.medium"
+  instance_type   = "t2.micro"
   key_name        = "${var.keyname}"
   #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
   #name            = "${var.name}"
-  user_data = "${file("install_jenkins.sh")}"
+  user_data = file("install_jenkins.sh")
 
   associate_public_ip_address = true
   tags = {
--- terraform/install_kubectl_hal.sh
+++ terraform/install_kubectl_hal.sh
@@ -9,7 +9,7 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
-yum install -y kubectl
+sudo yum install -y kubectl
 
-curl -L https://get.armory.io/halyard/install/latest/macos/InstallArmoryHalyard.sh > InstallArmoryHalyard.sh 
+sudo curl -L https://get.armory.io/halyard/install/latest/macos/InstallArmoryHalyard.sh > InstallArmoryHalyard.sh 
 sudo bash InstallArmoryHalyard.sh --version latest
